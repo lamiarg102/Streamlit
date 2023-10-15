be8c43152f06a55594b40e8139647497b4a272c9
lamia rouag <79458214+lamiarg102@users.noreply.github.com>
2023-10-15 15:07:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,32 +10,23 @@ from tensorflow.keras.models import load_model
 # Charger le modèle pré-entraîné
 model = load_model('model_facial_skin_MobileNetv2.h5')  # Assurez-vous que 'weights.h5' pointe vers votre modèle sauvegardé
 
-# Inside the predict function
+# Définir les classes de problèmes possibles
 def predict(img):
     # Faire la prédiction
     prediction = model.predict(img)
 
-    # Print the prediction values for debugging
-    print("Prediction values:", prediction)
-
-    # Trouver la classe avec la plus haute probabilité
-    argmax = np.argmax(prediction)
-    
     # Afficher les probabilités pour chaque classe
     st.subheader("Probabilités de chaque problème :")
     classes = ['blackhead', 'acne', 'ride0']
+
     for i in range(len(classes)):
         st.write(f"{classes[i]} : {prediction[0][i]}")
 
-    # Print the argmax value for debugging
-    print("Argmax value:", argmax)
-
-    # Afficher la classe prédite
-    if 0 <= argmax < len(classes):
-        predicted_class = classes[argmax]
-        st.write(f"Classe prédite : {predicted_class}")
-    else:
-        st.write("Classe prédite : Classe inconnue")
+    # Trouver la classe avec la plus haute probabilité
+    argmax = np.argmax(prediction)
+    predicted_class = classes[argmax]
+    
+    st.write(f"Classe prédite : {predicted_class}")
         
 # Titre de l'application Streamlit
 st.title("Welcome To Gerajeune App")
